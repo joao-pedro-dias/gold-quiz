052e820f03a8ba7ac24d61b220c6a11392826ded
moishawa <jpdias1999z@gmail.com>
2021-01-26 21:31:49 -0300

    Aula 2 - 26/01

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head'; // está vindo da pasta node_modules, onde já vem todas as dependências do nextJS. Por isso se chama biblioteca, vem tudo junto
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../scr/components/Widget';
 import QuizLogo from '../scr/components/QuizLogo';
@@ -25,8 +29,15 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState(''); // ou seja, estou falando que o estado inicial do meu input é vazio. E quando altera o estado temos o setName
+  console.log(name, setName);
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz - GoldSystem</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -34,7 +45,24 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault(); // retirar o evento padrão desta function
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(name);
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Informe seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
