41d8bc93e687aa5d54610b5e22686c6f4a2eb868
moishawa <jpdias1999z@gmail.com>
2021-01-30 00:01:43 -0300

    Aula 5 - 29/01

---
+++ src/components/QuizLogo/index.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Image from 'next/image';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Logo() {
+  return (
+    <Image
+      src="/Logo2.png"
+      alt="Pokémon Quiz!"
+      width={350}
+      height={164}
+    />
+  );
+}
+
+Logo.propTypes = {
+  // eslint-disable-next-line react/no-unused-prop-types
+  className: PropTypes.string.isRequired,
+};
+
+const QuizLogo = styled(Logo)`
+  margin: auto;
+  display: block;
+  @media screen and (max-width: 500px) {
+    margin: 0;
+  }
+`;
+
+export default QuizLogo;
--- src/screens/Quiz/index.js
+++ src/screens/Quiz/index.js
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/QuizContainer';
-import AlternativesForm from '../src/components/AlternativesForm';
-import Button from '../src/components/Button';
+import { Lottie } from '@crello/react-lottie';
+// import db from '../../../db.json';
+import Widget from '../../components/Widget';
+import QuizLogo from '../../components/QuizLogo';
+import QuizBackground from '../../components/QuizBackground';
+import QuizContainer from '../../components/QuizContainer';
+import AlternativesForm from '../../components/AlternativesForm';
+import Button from '../../components/Button';
+import BackLinkArrow from '../../components/BackLinkArrow';
 
+import loadingAnimation from './animate/loading.json';
+
+// eslint-disable-next-line react/prop-types
 function ResultWidget({ results }) {
   return (
     <Widget>
@@ -26,13 +32,14 @@ function ResultWidget({ results }) {
             }
             return somatoriaAtual;
           }, 0)} */}
+          {/* eslint-disable-next-line react/prop-types */}
           {results.filter((x) => x).length}
           {' '}
           perguntas
         </p>
         <ul>
           {results.map((result, index) => (
-            <li key={`result__${result}`}>
+            <li key={`result__${index}`}>
               #
               {index + 1}
               {' '}
@@ -55,8 +62,13 @@ function LoadingWidget() {
         Carregando...
       </Widget.Header>
 
-      <Widget.Content>
-        [Desafio do Loading]
+      <Widget.Content style={{ display: 'flex', justifyContent: 'center' }}>
+        <Lottie
+          width="200px"
+          height="200px"
+          className="lottie-container basic"
+          config={{ animationData: loadingAnimation, loop: true, autoplay: true }}
+        />
       </Widget.Content>
     </Widget>
   );
@@ -78,7 +90,7 @@ function QuestionWidget({
   return (
     <Widget>
       <Widget.Header>
-        {/* <BackLinkArrow href="/" /> */}
+        <BackLinkArrow href="/" />
         <h3>
           {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
         </h3>
@@ -110,7 +122,7 @@ function QuestionWidget({
               onSubmit();
               setIsQuestionSubmited(false);
               setSelectedAlternative(undefined);
-            }, 2 * 1000);
+            }, 3 * 1000);
           }}
         >
           {question.alternatives.map((alternative, alternativeIndex) => {
@@ -156,13 +168,14 @@ const screenStates = {
   LOADING: 'LOADING',
   RESULT: 'RESULT',
 };
-export default function QuizPage() {
+export default function QuizPage({ externalQuestions, externalBg }) {
   const [screenState, setScreenState] = React.useState(screenStates.LOADING);
   const [results, setResults] = React.useState([]);
-  const totalQuestions = db.questions.length;
   const [currentQuestion, setCurrentQuestion] = React.useState(0);
   const questionIndex = currentQuestion;
-  const question = db.questions[questionIndex];
+  const question = externalQuestions[questionIndex];
+  const totalQuestions = externalQuestions.length;
+  const bg = externalBg;
 
   function addResult(result) {
     // results.push(result);
@@ -179,8 +192,8 @@ export default function QuizPage() {
   React.useEffect(() => {
     // fetch() ...
     setTimeout(() => {
-      setScreenState(screenStates.QUIZ); // alterando o estado de loading para QUIZ...
-    }, 1 * 1000);
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 2000);
   // nasce === didMount
   }, []);
 
@@ -194,7 +207,7 @@ export default function QuizPage() {
   }
 
   return (
-    <QuizBackground backgroundImage={db.bg}>
+    <QuizBackground backgroundImage={bg}>
       <QuizContainer>
         <QuizLogo />
         {screenState === screenStates.QUIZ && (
